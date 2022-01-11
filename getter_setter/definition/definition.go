aa7cc1502f0d58ea1249408e1251d8b69c01a8a5
Marcos <mv.piesske@gmail.com>
2022-01-10 21:54:01 -0300

    Implementing generators

--- getter_setter/definition/definition.go
+++ getter_setter/definition/definition.go
@@ -19,12 +19,32 @@ func NewDefinition(file gs.File) Definition {
 	return Definition{File: file}
 }
 
-func (d Definition) GenFunctionGetByExtension() (error, []byte) {
-	// TODO: Implement this function
-	return nil, nil
+func (d Definition) GenFunctionGetByExtension() ([]byte, error) {
+
+	gphp, err := d.GettersPhp()
+
+	if err != nil {
+		return nil, err
+	}
+
+	languages := map[string][]byte{
+		"php": gphp,
+	}
+
+	return languages[d.File.Language], nil
 }
 
-func (d Definition) GenFunctionSetByExtension() (error, []byte) {
-	// TODO: Implement this function
-	return nil, nil
+func (d Definition) GenFunctionSetByExtension() ([]byte, error) {
+
+	sphp, err := d.SettersPhp()
+
+	if err != nil {
+		return nil, err
+	}
+
+	languages := map[string][]byte{
+		"php": sphp,
+	}
+
+	return languages[d.File.Language], nil
 }
