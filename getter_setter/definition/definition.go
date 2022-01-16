e9284be3a0cbe074de84d27b66777db0910f0288
Marcos <mv.piesske@gmail.com>
2022-01-16 11:00:22 -0300

    Add the definition Attributes func to definition pkg

--- getter_setter/definition/definition.go
+++ getter_setter/definition/definition.go
@@ -1,8 +1,17 @@
 package definition
 
-import fgs "github.com/marcos-dev88/go-getter-setter/getter_setter/file_gs"
+import (
+	"regexp"
+	"strings"
+
+	fgs "github.com/marcos-dev88/go-getter-setter/getter_setter/file_gs"
+)
 
 type (
+	Definitions interface {
+		DefineFileGsAttributes() error
+	}
+
 	GenerateFunction interface {
 		GenFunctionGetByLanguage() (error, []byte)
 		GenFunctionSetByLanguage() (error, []byte)
@@ -47,4 +56,31 @@ func (d Definition) GenFunctionSetByLanguage() ([]byte, error) {
 	}
 
 	return languages[d.File.Language], nil
+}
+
+func (d *Definition) DefineFileGsAttributes() error {
+	var regexAttr = regexp.MustCompile(`var_name: (\S+) - type: (\S+)`)
+
+	attrs, err := d.File.GetFileAttributes()
+	var attributesStringArr = strings.Split(string(attrs), "|")
+
+	var attrArr []fgs.Attribute
+
+	if err != nil {
+		return err
+
+	}
+
+	for _, v := range attributesStringArr {
+
+		attrMatch := regexAttr.FindStringSubmatch(v)
+
+		if attrMatch != nil {
+			attrArr = append(attrArr, fgs.NewAttribute(attrMatch[1], attrMatch[2]))
+		}
+	}
+
+	d.File.Attributes = attrArr
+
+	return nil
 }
