2d8fd5d1881b1dfd7e6e767fc93c7130b1ce895a
Marcos <mv.piesske@gmail.com>
2022-02-07 21:30:36 -0300

    Updating file_gs to have a new param, to check which kind of function it will generate

--- getter_setter/file_gs/file_gs.go
+++ getter_setter/file_gs/file_gs.go
@@ -6,15 +6,17 @@ type FileGs struct {
 	Path       string      `json:"path"`
 	Language   string      `json:"language"`
 	Visibility string      `json:"visibility"`
+	Functions  string      `json:"functions"`
 	Attributes []Attribute `json:"attributes"`
 	Logger     logger.Logging
 }
 
-func NewFileGs(path, language, visibility string, attributes []Attribute, log logger.Logging) FileGs {
+func NewFileGs(path, language, visibility, functions string, attributes []Attribute, log logger.Logging) FileGs {
 	return FileGs{
 		Path:       path,
 		Language:   language,
 		Visibility: visibility,
+		Functions:  functions,
 		Attributes: attributes,
 		Logger:     log,
 	}
